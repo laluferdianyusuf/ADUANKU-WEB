e2282ad77ee2d6a7901559e08dade2ac6675adda
laluoki <you@example.com>
2025-03-26 17:19:16 +0800

    adding open detail information detail

--- src/components/card/card-information.tsx
+++ src/components/card/card-information.tsx
@@ -34,22 +34,30 @@ const data = [
     desc: "Lorem",
   },
 ];
+interface CardInformationProps {
+  onClick: () => void;
+}
 
-export default function CardInformation() {
+export default function CardInformation({ onClick }: CardInformationProps) {
   return (
     <div className="grid grid-cols-3 items-start gap-4">
       {data.map((item, index) => (
-        <Card key={index}>
+        <Card onClick={onClick} key={index}>
           <CardHeader>
             <CardTitle>{item.title}</CardTitle>
             <CardDescription>{item.desc}</CardDescription>
           </CardHeader>
           <CardContent>
-            <p>Card Content</p>
+            <p className="text-justify truncate">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
+              harum dignissimos quisquam fuga eum. Aspernatur neque deleniti
+              sequi delectus aperiam error vero, recusandae quasi. Perspiciatis
+              voluptas nobis magni in fugit corporis reiciendis. A suscipit
+              accusantium temporibus dolorem consequatur asperiores praesentium,
+              numquam rem nihil perspiciatis quos nam nemo optio delectus
+              dolore?
+            </p>
           </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
         </Card>
       ))}
     </div>
