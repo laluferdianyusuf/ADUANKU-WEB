f6c7d56e736b89318a1cac24395b6604dee08225
laluoki <you@example.com>
2025-06-04 14:52:25 +0800

    [oki] adding any features to components

--- src/components/form/form-custom.tsx
+++ src/components/form/form-custom.tsx
@@ -15,6 +15,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { SquarePlus } from "lucide-react";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -33,28 +34,75 @@ export function FormCustom() {
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     console.log(values);
   };
-
+  const variable = [
+    {
+      id: "identitas_pengadu",
+      name: "Identitas Pengadu",
+      color: "bg-blue-300",
+    },
+    {
+      id: "identitas_pendamping",
+      name: "Identitas Pendamping",
+      color: "bg-red-300",
+    },
+    {
+      id: "identitas_korban",
+      name: "Identitas Korban",
+      color: "bg-green-300",
+    },
+    {
+      id: "identitas_pelaku",
+      name: "Identitas Pelaku",
+      color: "bg-yellow-300",
+    },
+    {
+      id: "kronologi",
+      name: "Kronologi",
+      color: "bg-orange-300",
+    },
+  ];
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    // <Form {...form}>
+    //   <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+    //     <FormField
+    //       control={form.control}
+    //       name="username"
+    //       render={({ field }) => (
+    //         <FormItem>
+    //           <FormLabel>Username</FormLabel>
+    //           <FormControl>
+    //             <Input placeholder="shadcn" {...field} />
+    //           </FormControl>
+    //           <FormDescription>
+    //             This is your public display name.
+    //           </FormDescription>
+    //           <FormMessage />
+    //         </FormItem>
+    //       )}
+    //     />
+    //     <Button type="submit">Submit</Button>
+    //   </form>
+    // </Form>
+
+    <div className="grid grid-cols-3 gap-2">
+      {variable.map((items, idx) => (
+        <div
+          key={idx}
+          className={`${items.color} p-2 border outline-black rounded-md h-[20vh]`}
+        >
+          <div
+            className={`h-full  flex flex-col items-center justify-center font-semibold`}
+          >
+            {items.name}
+            <button className="flex-1 self-end mt-16 mr-2 p-1 rounded-sm bg-cyan-700">
+              <SquarePlus size={30} />
+            </button>
+            <p className="mt-4 italic text-xs capitalize flex self-start ml-3 font-normal">
+              * Masukkan detail identitas
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
